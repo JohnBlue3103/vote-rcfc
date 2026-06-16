5b516f53d06dbae57cade7fcdb60e40bc4ea673c
John Bernard <john.blue3103@gmail.com>
2026-06-16 18:29:26 +0200

    config: clés Supabase vote-rcfc

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Settings > API dans le dashboard Supabase (projet vote-rcfc)
-const SUPABASE_URL  = 'REMPLACE_PAR_TON_URL';
-const SUPABASE_ANON = 'REMPLACE_PAR_TA_CLE_ANON';
+const SUPABASE_URL  = 'https://jsajbiqptxgiaqaxrdew.supabase.co';
+const SUPABASE_ANON = 'sb_publishable_gF_-ZSfhw_Doz6ZbbqJ9kA_AWTAISGA';
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
